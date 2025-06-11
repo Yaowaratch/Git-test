809432e0df5a90d488c95d3a5ddb517cb308a7a6
acarenarin <acarewishaya@hotmail.com>
2025-06-11 10:38:33 +0700

    03

--- hello.py
+++ hello.py
@@ -1,2 +1,9 @@
 print('Hello, Feature branch')
 print('o3o')
+name= input()
+count = int(input('Enter a number: '))
+for i in range(count):
+    print(f'Hello, {name} {i+1}')   
+print('End of the program')
+print('End of the program') 
+print('End of the program')
